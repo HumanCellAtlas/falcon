3e917a7798c3a899e623453e2e20c08616c503ee
Rex Wang <chengche@broadinstitute.org>
2018-07-19 16:03:56 -0400

    Make the main function more clear.

--- falcon/__main__.py
+++ falcon/__main__.py
@@ -4,11 +4,13 @@ import os
 
 
 if __name__ == '__main__':
-    handler = Queue_Handler(os.environ.get('CONFIG_PATH'))
-    igniter = Igniter(os.environ.get('CONFIG_PATH'))
+    config_path = os.environ.get('CONFIG_PATH')
+    handler = Queue_Handler(config_path)
+    igniter = Igniter(config_path)
 
     handler.spawn_and_run()
     igniter.spawn_and_run(handler.workflow_queue)
 
+    # without monitoring the health of the 2 processes, these joins are trivial
     handler.join()
     igniter.join()
