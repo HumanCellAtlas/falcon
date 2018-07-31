80b0c3861ff3c9e0cd5681385f93a038ee304572
Rex Wang <chengche@broadinstitute.org>
2018-07-31 11:53:53 -0400

    Refactor the __main__ according to the changes to the queue_handler.

--- falcon/__main__.py
+++ falcon/__main__.py
@@ -1,15 +1,15 @@
-from falcon.queue_handler import Queue_Handler
+from falcon.queue_handler import QueueHandler
 from falcon.igniter import Igniter
 import os
 
 
 if __name__ == '__main__':
     config_path = os.environ.get('CONFIG_PATH')
-    handler = Queue_Handler(config_path)  # instantiate a concrete queue_handler
-    igniter = Igniter(config_path)  # instantiate a concrete igniter
+    handler = QueueHandler(config_path)  # instantiate a concrete `QueueHandler`
+    igniter = Igniter(config_path)  # instantiate a concrete `Igniter`
 
-    handler.spawn_and_start()  # start the thread of the queue_handler
-    igniter.spawn_and_start(handler)  # start the thread of the igniter by passing the queue_handler into it
+    handler.spawn_and_start()  # start the thread within the handler
+    igniter.spawn_and_start(handler)  # start the thread within the igniter by passing the handler into it
 
     # without monitoring the health of the 2 processes, these joins are trivial
     handler.join()
