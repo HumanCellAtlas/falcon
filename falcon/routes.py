94202af982abcf89d54fbbd6a6c111d6b41610e9
Saman Ehsan <se8ea@virginia.edu>
2019-03-26 13:34:23 -0400

    Simplify check for thread

--- falcon/routes.py
+++ falcon/routes.py
@@ -10,7 +10,7 @@ def status():
     active_falcon_threads = {}
     for falcon_thread_name in FALCON_THREAD_NAMES:
         thread = active_threads.get(falcon_thread_name)
-        if falcon_thread_name not in active_threads.keys():
+        if not thread:
             abort(500)
         elif not thread.is_alive():
             abort(500)
